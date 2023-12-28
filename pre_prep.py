bd5b57ff5958fd2259b00c9bdcd01e6536e8c700
haticeyigit <haticeyiggt@gmail.com>
2023-12-28 13:54:36 +0300

    pre_prep eklendi

---
+++ pre_prep.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from googleapiclient.discovery import build
+from IPython.display import JSON
+
+from config import YOUTUBE_API_KEY
+from methods import get_channel_stats, get_video_ids
+
+
+
+
+
+
+
+
+
+
+
